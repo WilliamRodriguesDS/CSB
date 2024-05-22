1312b7c8bef637c9b15be9754e215764b2fef758
William05DEV <williamrodriguesdeoliveira22@gmail.com>
2024-05-22 19:30:14 -0300

    2.0v

---
+++ script.js
@@ -0,0 +1,11 @@
+function calcular(){
+
+    const salario_base = parseFloat(document.getElementById('base').value);
+    const gratificações  = parseFloat(document.getElementById('grat').value);
+    const adicionais  = parseFloat(document.getElementById('add').value);
+    const extras  = parseFloat(document.getElementById('extras').value);
+    const por_hora = parseFloat(document.getElementById("hora").value);
+
+    const resultado = salario_base + gratificações + adicionais + (extras * por_hora);
+    document.getElementById('resultado').innerHTML= "R$" + " " + resultado.toFixed(2);
+    }
